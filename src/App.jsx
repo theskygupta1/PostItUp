f2478d87d580227274cac8810fd6e0a29b549e3e
Akash Kumar <akashkumar3429@gmail.com>
2025-12-12 02:03:22 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
-import Header from './components/header';
+import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import CreatePost from './components/CreatePost';
