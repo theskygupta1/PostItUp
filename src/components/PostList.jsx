d3f1ecd2a77112666b3ab654d0b4b69534178d31
Akash Kumar <akashkumar3429@gmail.com>
2025-12-12 02:33:13 +0530

    Updated features

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,14 @@
-import { useContext } from "react";
+import { useContext,useState } from "react";
 import Post from "./Post"
 import {PostList as PostListData} from "../store/post-list-store"
 import WelcomeMsg from "./WelcomeMsg";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
-    const { postList,addInitialPosts } = useContext(PostListData)
+    const { postList, addInitialPosts } = useContext(PostListData)
+    const [ fetching, setFetching ] = useState(false);
     const handleGetPostsClick = () => {
+        setFetching(true)
         fetch('https://dummyjson.com/posts')
             .then(res => res.json())
             .then(data => {
@@ -17,11 +20,13 @@ const PostList = () => {
                         userId: String(p.userId)
                     }))
                 )
+                setFetching(false);
             })
     }
     return <>
-        {postList.length===0 && <WelcomeMsg onGetPostsClick={handleGetPostsClick}></WelcomeMsg>}
-        {postList.map((post) => (
+        {fetching && <LoadingSpinner/>}
+        {!fetching && postList.length===0 && <WelcomeMsg onGetPostsClick={handleGetPostsClick}></WelcomeMsg>}
+        {!fetching && postList.map((post) => (
             <Post key={post.id} post={post} />
         ))}
     </>
